56992f96e6a472208aaa064c238ab1de32452c8b
Minuke <nakamadeath@gmail.com>
2024-08-04 14:16:14 +0200

    set level of difficulty visually on the cards

---
+++ src/app/core/models/enums/colour.enum.ts
@@ -0,0 +1,6 @@
+export enum Colour {
+	EASY = '#66BB6A',
+	INTERMEDIATE = '#FFA726',
+	ADVANCED = '#EF5350',
+	DEFAULT = '#FFFFFF'
+}
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { Colour } from '@core/models/enums/colour.enum';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
 
+    public color  =  input.required<string>();
+
+    get cardColor(): string {
+      switch (this.color()) {
+        case 'easy':
+          return Colour.EASY; 
+        case 'intermediate':
+          return Colour.INTERMEDIATE;
+        case 'advanced':
+          return Colour.ADVANCED;
+        default:
+          return Colour.DEFAULT;
+      }
+    }
 }
